7124b6fdc8439b336b6b042672824ce8e0c11cd2
ccindq <cc199236@163.com>
2021-06-25 08:35:29 +0800

    增加限流配置

--- c-api-gateway/src/main/java/com/cc/project/gateway/GatewayApplication.java
+++ c-api-gateway/src/main/java/com/cc/project/gateway/GatewayApplication.java
@@ -6,13 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
-@EnableFeignClients
-@EnableSwagger2
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class GatewayApplication {
 
